4ab93f5501c55a15166eaca34596f5a97ec35200
Ben Cuan <github@bencuan.me>
2021-01-08 14:08:25 -0800

    Add certbot challenge

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   );
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
-});
+}, express.static(__dirname, { dotfiles: 'allow' }));
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
